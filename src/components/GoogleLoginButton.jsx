3c4f6738aca3082285a29f5d92501c99c7d11bb7
Cristian Alvarado <calvarado@upre.gob.bo>
2025-11-13 16:59:58 -0400

    login95%

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { UserAuth } from '../context/AuthContext';
 
 export default function GoogleLoginButton() {
+  const { login } = UserAuth();
   useEffect(() => {
     /* global google */
     if (!window.google) return;
@@ -13,16 +14,16 @@ export default function GoogleLoginButton() {
       theme: 'filled_black',
       size: 'large'
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function handleCredentialResponse(response) {
     // response.credential es el id_token de Google
-    const { login } = UserAuth();
-    const id_token = response.credential;
+    // const id_token = response.credential;
     // Enviar al backend para verificar y crear usuario
     console.log('el response',response);
 
-    await login({operacion:'G',user:response,pass:id_token});
+    await login({operacion:'G',user:response.credential,pass:response.clientId});
     return;
     
     // const res = await fetch('/auth/google', {
